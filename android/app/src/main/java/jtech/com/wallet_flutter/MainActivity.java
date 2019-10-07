a69d61c16f0d8ebb3182c7aef858dca0bcc31d8e
Jatin Kumar Jain <31545613+Jatzj7@users.noreply.github.com>
2019-10-07 14:10:54 +0530

    LogIn Page

---
+++ android/app/src/main/java/jtech/com/wallet_flutter/MainActivity.java
@@ -0,0 +1,13 @@
+package jtech.com.wallet_flutter;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
